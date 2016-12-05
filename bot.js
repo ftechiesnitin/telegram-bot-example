7531a273b71ac3a5c1e28cd0b2631a37dfbf837e
Nitin Dolli <nitin.dolli@gmail.com>
2016-12-06 01:44:49 +0800

    bot code initated

---
+++ bot.js
@@ -0,0 +1,18 @@
+var Telegraf = require('telegraf');
+
+// var app = new Telegraf('226441370:AAHycQ7ELMKWN8IAlKbcPZ4CmncKq_Kedbs');
+//
+// app.command('start', function (ctx) {
+//   console.log('start', ctx.from);
+//   ctx.reply('Welcome!');
+// });
+//
+// app.hears('hi', function (ctx) {
+//   ctx.reply('Hey there!');
+// });
+// app.on('sticker', function (ctx) {
+//  ctx.reply('👍');
+// });
+console.log('hello');
+
+// app.startPolling();
