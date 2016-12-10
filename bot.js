3e1dd941695e42cc6c204eaa364936f899cca40c
Nitin Dolli <nitin.dolli@gmail.com>
2016-12-10 14:22:51 +0800

    conditional smaple bot created

--- bot.js
+++ bot.js
@@ -1,18 +1,17 @@
-var Telegraf = require('telegraf');
+// Require telegram-bot-api
+var TelegramBot = require('node-telegram-bot-api');
+// Create a bot that uses 'polling' to fetch new updates
+var bot = new TelegramBot('226441370:AAHycQ7ELMKWN8IAlKbcPZ4CmncKq_Kedbs', { polling: true });
 
-// var app = new Telegraf('226441370:AAHycQ7ELMKWN8IAlKbcPZ4CmncKq_Kedbs');
-//
-// app.command('start', function (ctx) {
-//   console.log('start', ctx.from);
-//   ctx.reply('Welcome!');
-// });
-//
-// app.hears('hi', function (ctx) {
-//   ctx.reply('Hey there!');
-// });
-// app.on('sticker', function (ctx) {
-//  ctx.reply('👍');
-// });
-console.log('hello');
+// Listen for any kind of message. There are different kinds of
+// messages.
+bot.on('message', function (msg) {
+  var chatId = msg.chat.id;
 
-// app.startPolling();
+  // send a message to the chat acknowledging receipt of their message
+  var message = msg.text.toLowerCase();
+
+  if(message.indexOf('hi') > -1 || message.indexOf('hello') > -1 || message.indexOf('hey') > -1) return bot.sendMessage(chatId, 'Hey Sweet thing, how you doing?');
+  if(message.indexOf('age') > -1 || message.indexOf('old') > -1) return bot.sendMessage(chatId, 'I don\'t really have an age. I guess you could say I\'m young at heart.');
+  if(message.indexOf('love') > -1 || message.indexOf('like') > -1) return bot.sendVideo(chatId, '/Users/nitinmegamind/Desktop/giphy.gif');
+});
